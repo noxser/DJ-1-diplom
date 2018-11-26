20aba7281c6a60ead932c7c265d48a9cfdffc2f4
Noxser <noxser@gmail.com>
2018-11-26 19:10:12 +0500

    Поправил урлы

--- graduation_work_v2/cart/urls.py
+++ graduation_work_v2/cart/urls.py
@@ -1,9 +1,10 @@
 from .views import cart_detail, cart_add, cart_remove
 from django.urls import path
+from django.conf.urls import url
 
 app_name = 'cart'
 urlpatterns = [
     path('', cart_detail, name='main'),
-    path('add/(<merchandise_id>[-\w\d]+)/', cart_add, name='cart_add'),
-    path('remove/(<merchandise_id>[-\w\d]+)/', cart_remove, name='cart_remove'),
+    url(r'^add/(?P<merchandise_id>[-\w\d]+)/$', cart_add, name='cart_add'),
+    url(r'^remove/(?P<merchandise_id>[-\w\d]+)/$', cart_remove, name='cart_remove'),
 ]
